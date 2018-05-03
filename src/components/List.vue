1751f944d73be7cc9f514a76c527edc62d5875af
WWK563388548 <wc563388548@gmail.com>
2018-05-03 10:52:58 +0900

    updated list.vue

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,7 +1,15 @@
 <!-- HTML -->
 <template>
     <div class="view">
-        <h1>{{title}}</h1>
+        <h1 class="title">{{title}}</h1>
+
+        <ul>
+            <!-- 使用v-for循环渲染事项 -->
+            <!-- 使用v-bind判断是否完成事项 -->
+            <li v-for="item in items" v-bind:class="{finish: item.isFinished}">
+                {{item.text}}
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -14,6 +22,16 @@
         data(){
             return {
                 title: "我的待办事项",
+                items: [
+                    {
+                       text: "使用Vue开发项目",
+                       isFinished: false
+                    },
+                    {
+                       text: "阅读Vue文档",
+                       isFinished: true
+                    },
+                ],
             }
         }
     }
@@ -25,5 +43,19 @@
         width: 400px;
         border: 1px solid gray;
         margin: 20px auto;
+    }
+
+    .view .title{
+        border-bottom: 1px solid gray;
+    }
+
+    .view ul li{
+        margin: 10px 0;
+        list-style: none;
+    }
+
+    .view .finish{
+        color: grey;
+        text-decoration: line-through;
     }
 </style>
