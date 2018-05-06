7d3874f9693205bb89b2b7605b438e26170a507c
WWK563388548 <wc563388548@gmail.com>
2018-05-06 10:09:52 +0900

    made some changes and using props for parent => child communication

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <template>
   <div id="app">
+    <!-- 3.使用组件 -->
+    <!-- 第二步：向该组件传递数据（父组件app.vue传递值给子组件Header.vue） -->
+    <HeaderView msg="待办事项" />
+
     <img src="./assets/logo.png">
     <!-- 3.使用组件 -->
-    <ListView></ListView>
+    <ListView />
   </div>
 </template>
 
 <script>
 // 1.倒入组件
+// List.vue是App.vue的子组件（App.vue是父组件）
+// 父子组件之间的通信问题（组件间通信）
 import ListView from './components/List'
+import HeaderView from './components/header'
 
 export default {
   name: 'App',
   components: {
     // 2.注册组件
-    ListView
+    ListView, HeaderView
   }
 }
 </script>
---
+++ src/components/header.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="headerview">
+        <h1>{{msg}}</h1>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                title: "标题部分..."
+            }
+        }, // 父向子传参第一步：添加props
+        props: {
+            msg:{ // 这里可以看作是自定义标签属性
+                // 设置传输数据类型
+                type: String,
+                default: "默认值"
+
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
